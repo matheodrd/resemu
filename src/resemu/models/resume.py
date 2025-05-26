0dab984db3d698bb2602d56653b1f762a5b80dd7
matheodrd <matheodurand14@gmail.com>
2025-05-27 00:23:32 +0200

    feat: add Resume pydantic models

---
+++ src/resemu/models/resume.py
@@ -0,0 +1,46 @@
+from typing import List
+from datetime import date
+
+from pydantic import BaseModel, HttpUrl, EmailStr
+
+
+class Contact(BaseModel):
+    name: str
+    email: EmailStr
+    portfolio: HttpUrl | None = None
+    github: HttpUrl | None = None
+
+
+class SkillCategory(BaseModel):
+    category: str  # e.g. "Frameworks", "Languages"
+    skills: List[str]
+
+
+class Experience(BaseModel):
+    title: str
+    company: str
+    location: str
+    start_date: date
+    end_date: date | None = None
+    bullets: List[str]
+
+
+class Project(BaseModel):
+    title: str
+    url: HttpUrl | None = None
+    bullets: List[str]
+
+
+class Education(BaseModel):
+    school: str
+    degree: str
+    field: str
+    graduation_date: date
+
+
+class Resume(BaseModel):
+    contact: Contact
+    skills: List[SkillCategory]
+    experience: List[Experience]
+    projects: List[Project]
+    education: List[Education]
